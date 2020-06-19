d8eccc34afdeafb80abfd9a321d4dd1a9d715bda
tahirbhalli <tahirbhalli16@gmail.com>
2020-06-19 18:16:24 +0500

    add element

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function node() {
+   let div= document.getElementById('main')
+   var b=document.createElement('h2')
+   b.className='blue-color'
+   b.innerText='this is ad through java script'
+   div.appendChild(b)
+}
+function fnt(params) {
+    
+}
